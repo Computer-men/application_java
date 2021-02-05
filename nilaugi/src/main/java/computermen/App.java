a0bea8a7dc6e4a24dc4ef00eb3c3b302e5f4fb2f
lucasduhamel <77484627+lucasduhamel@users.noreply.github.com>
2021-02-05 11:56:58 +0100

    JavaFX avec MYSQL
    JavaFx avec connexion vers base de donnée SQL

--- nilaugi/src/main/java/computermen/App.java
+++ nilaugi/src/main/java/computermen/App.java
@@ -5,6 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Statement;
+
 
 import java.io.IOException;
 
@@ -20,6 +29,33 @@ public class App extends Application {
         scene = new Scene(loadFXML("ConnexionF"), 600, 600);
         stage.setScene(scene);
         stage.show();
+        String dbURL = "jdbc:mysql://localhost:3306/ap2_gsb";
+        String username = "root";
+        String password = "root";
+         
+        try {
+         
+            Connection conn = DriverManager.getConnection(dbURL, username, password);
+         
+            if (conn != null) {
+                System.out.println("Connected !");
+                String sql = "SELECT * FROM ap2_gsb.agents ";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                ResultSet result = statement.executeQuery(sql);
+                int count = 0;
+                // statement.setString(1, "4");
+                while (result.next()) {
+                    String us_nom = result.getString(2);
+                    String us_prenom = result.getString(3);
+                    String us_profession = result.getString(4);
+                    String output = "User #%d: %s - %s - %s";
+                    System.out.println(String.format(output, ++count, us_nom, us_prenom, us_profession));
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Error connect");
+        }
     }
 
     static void setRoot(String fxml) throws IOException {
--- nilaugi/src/main/java/module-info.java
+++ nilaugi/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module computermen {
     requires javafx.controls;
     requires javafx.fxml;
-    
+    requires java.sql;
+    requires mysql.connector.java;
     opens computermen to javafx.fxml;
     exports computermen;
 }
